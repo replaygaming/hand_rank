a2b27077b101044a296a043ec76e63a0e96020ea
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2015-09-15 12:10:43 +0200

    Adds shim for transforming input to simple array of ints.

--- lib/hand_rank.rb
+++ lib/hand_rank.rb
@@ -2,6 +2,11 @@ require "hand_rank/version"
 
 module HandRank
   HOME = __FILE__.to_s.gsub('.rb','/')
+
+  def self.get( hand )
+    cards = hand.to_a.map{|c| c.to_i }
+    self.rank( cards )
+  end
 end
 
 require "hand_rank/hand_rank"
