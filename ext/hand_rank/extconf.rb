97e348a204b65f7b37b9ce2abe6d20fd17215334
John Tajima <manjiro@gmail.com>
2019-02-07 11:52:05 -0500

    Auto unzip the ranks.data.zip file and then deletes the .zip file

--- ext/hand_rank/extconf.rb
+++ ext/hand_rank/extconf.rb
@@ -1,3 +1,11 @@
 require "mkmf"
 $CFLAGS+=" -O3"
 create_makefile "hand_rank/hand_rank"
+puts "Done making file"
+
+
+puts "Unzipping zip data"
+curr_dir = File.dirname(__FILE__)
+zip_dir  = File.join(curr_dir, "../../lib/hand_rank/")
+system("unzip", File.join(zip_dir, "ranks.data.zip"), "-d", zip_dir)
+system('rm', File.join(zip_dir, "ranks.data.zip"))
